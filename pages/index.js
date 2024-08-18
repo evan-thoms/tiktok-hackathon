f63d1c0229c59eb0794480aa30ebd7a52e2c8bac
MudasserJalal <jalal.mud@gmail.com>
2024-08-18 02:34:23 -0600

    integration of Notif stystem(User<->Redis)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import io from 'socket.io-client';
 import Header from "../components/Header";
 import Card from "../components/Card";
 import Slider from "react-slick";
@@ -6,6 +7,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Footer from "../components/Footer";
 import Cookies from 'js-cookie';
+
 
 
 // Sample content data for the cards
@@ -69,6 +71,10 @@ const sliderSettings = {
 
 export default function Home() {
 
+    // State to control alert visibility
+    const [showAlert, setShowAlert] = useState(true);
+    const [notificationMessage, setNotificationMessage] = useState('');
+
   // get user from cookie
   var user_cookie = Cookies.get('user');
   const [title, setTitle] = useState('');
@@ -92,14 +98,66 @@ export default function Home() {
       username: "",
     }
   }
+
   console.log(user_info);
 
+  useEffect(() => {
+    
+    // Connect to the Socket.IO server
+    const socket = io("http://localhost:3001");
+    // Handle Socket.IO connection open
+    socket.on('connect', () => {
+      console.log('Socket.IO connected');
+    });
+
+    // Handle incoming messages
+    socket.on('notification', (message) => {
+      setNotificationMessage(message);
+      setShowAlert(true);
+      
+    });
+
+    // Handle Socket.IO errors
+    socket.on('error', (error) => {
+      console.error('Socket.IO error:', error);
+    });
+
+    // Handle Socket.IO connection close
+    socket.on('disconnect', () => {
+      console.log('Socket.IO connection closed');
+    });
+
+    // Clean up the socket connection on component unmount
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  // Function to handle closing the notification
+  const handleCloseNotification = () => {
+    setShowAlert(false);
+  };
 
   return (
     <>
       <div className="bg-customBeige">
         <Header />
 
+        <div>
+            {showAlert && 
+              <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+              <strong className="font-bold">New Content! </strong>
+              <span className="block sm:inline">A user just uploaded.</span>
+              <span className="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={handleCloseNotification}>
+      <svg className="fill-current h-6 w-6 text-red-500 cursor-pointer" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <title>Close</title>
+        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+      </svg>
+    </span>
+              </div>
+            }
+        </div>
+         
         {/* hero */}
         <section className="flex flex-col justify-center items-center py-40 p-6 rounded-lg">
           <h2 className=" text-center text-5xl mb-5 font-semibold text-gray-800">
@@ -129,7 +187,7 @@ export default function Home() {
             ))}
           </Slider>
         </section>
-
+\
 
         {/* submit content */}
         <section className="mt-10 mb-28 p-6 mt-28 bg-white rounded-lg max-w-lg mx-auto">
