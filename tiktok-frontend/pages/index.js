469e4c5c18d84be7e1f0fc3cd1d1b95418d2c49f
haru235 <harusakai0515@gmail.com>
2024-08-18 13:36:49 -0500

    notif msg

--- tiktok-frontend/pages/index.js
+++ tiktok-frontend/pages/index.js
@@ -182,8 +182,8 @@ export default function Home() {
         <div>
           {showAlert &&
             <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-              <strong className="font-bold">New Content! </strong>
-              <span className="block sm:inline">A user just uploaded.</span>
+              <strong className="font-bold">{notificationMessage}</strong>
+              <span className="block sm:inline">Go check it out!.</span>
               <span className="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={handleCloseNotification}>
                 <svg className="fill-current h-6 w-6 text-red-500 cursor-pointer" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                   <title>Close</title>
